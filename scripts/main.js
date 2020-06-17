f2de418e73f3a19343ae53bc545b40962c094d48
kochatkov <vlad.kochatkov@gmail.com>
2020-06-17 13:45:16 +0300

    Updates

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,20 @@
 'use strict';
+
+const menu = document.querySelector('.menu');
+const button = document.querySelector('.header__menu');
+const buttonToClose = document.querySelector('.menu__close');
+const menuLink = document.querySelectorAll('.menu__link');
+
+const closeMenu = () => {
+  menu.classList.remove('menu__show');
+};
+
+button.addEventListener('click', () => {
+  menu.classList.add('menu__show');
+});
+
+buttonToClose.addEventListener('click', closeMenu);
+
+menuLink.forEach(link => {
+  link.addEventListener('click', closeMenu);
+});
